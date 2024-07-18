8deb20febd1be93aecb8bab792733d1368577e0d
jtagrgh <jakob.koblinsky@gmail.com>
2024-07-18 17:48:02 -0400

    Add common data format header

---
+++ commons/data_formats.h
@@ -0,0 +1,25 @@
+#include <stdint.h>
+
+typedef struct BMI2SensAxisData
+{
+    /*! Data in x-axis */
+    int16_t x;
+
+    /*! Data in y-axis */
+    int16_t y;
+
+    /*! Data in z-axis */
+    int16_t z;
+} BMI2SensAxisData;
+
+typedef struct BMI2SensData
+{
+    /*! Accelerometer axes data */
+    struct BMI2SensAxisData acc;
+
+    /*! Gyroscope axes data */
+    struct BMI2SensAxisData gyr;
+
+    /*! Sensor time */
+    uint32_t sens_time;
+} BMI2SensData;
